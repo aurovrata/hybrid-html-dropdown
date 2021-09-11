b027717927e9f146378138bae7a82c8ce28a8786
aurovrata <vrata.venet@gmail.com>
2021-09-11 20:23:14 +0530

    v2.0.5

--- hybrid-dropdown.js
+++ hybrid-dropdown.js
@@ -1,6 +1,6 @@
 /*
 Hybrid Dropdown JavaScript plugin insprired from an original idea by Sandrina Pereira (twitter:@a_sandrina_p)
-Version: 2.0.3
+Version: 2.0.5
 Authors: Aurovrata Venet
 Twitter: @aurovrata
 GitHub: https://github.com/aurovrata/hybrid-html-dropdown
